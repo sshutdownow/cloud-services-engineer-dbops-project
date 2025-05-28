b94fb94f4a6cc4068da06ebe57e7846e42e7a360
Igor Popov <sshutdownow@users.noreply.github.com>
2025-05-29 00:30:32 +0200

    Update V004__create_index.sql

--- migrations/V004__create_index.sql
+++ migrations/V004__create_index.sql
@@ -1,2 +1,3 @@
 CREATE INDEX order_product_order_id_idx ON order_product(order_id); 
+CREATE INDEX order_product_quantity_idx ON order_product(quantity); -- sum
 CREATE INDEX orders_status_date_idx ON orders(status, date_created);
