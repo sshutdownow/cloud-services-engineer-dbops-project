3f477be42e145a2d3adc7fa5d847fbc6ee19937e
Igor Popov <sshutdownow@users.noreply.github.com>
2025-07-11 02:24:25 +0200

    Update V003__insert_data.sql

--- migrations/V003__insert_data.sql
+++ migrations/V003__insert_data.sql
@@ -15,5 +15,8 @@ INSERT INTO product_info (product_id, name, price) VALUES (5, 'Мюнхенск�
 INSERT INTO product_info (product_id, name, price) VALUES (6, 'Русская', 189.00);
 */
 INSERT INTO orders (id, status, date_created) SELECT i, (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], DATE(NOW() - (random() * (NOW()+'90 days' - NOW()))) FROM generate_series(1, 10000000) s(i);
+-- ERROR: duplicate key value violates unique constraint "orders_pkey"
+-- Update the sequence https://stackoverflow.com/questions/244243/how-to-reset-postgres-primary-key-sequence-when-it-falls-out-of-sync
+SELECT setval('orders_id_seq', (SELECT GREATEST(MAX(id), nextval('orders_id_seq') - 1) FROM orders));
 -- INSERT INTO orders_date (order_id, status, date_created) SELECT i, (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], DATE(NOW() - (random() * (NOW()+'90 days' - NOW()))) FROM generate_series(1, 10000000) s(i);
 INSERT INTO order_product (quantity, order_id, product_id) SELECT floor(1+random()*50)::int, i, 1 + floor(random()*6)::int % 6 FROM generate_series(1, 10000000) s(i);
